dfce1a47ef4ca4101565db4e3cb1071be77bd3d8
Retkatmun <retkatmun1@gmail.com>
2025-09-17 19:56:40 +0100

    remove nav link flex

--- main.js
+++ main.js
@@ -19,3 +19,9 @@ overlay.addEventListener('click', closeMenu);
 document.querySelectorAll('.nav-links a').forEach(link => {
   link.addEventListener('click', closeMenu);
 });
+
+function copyAcct() {
+  var acct = document.getElementById("acct").innerText;
+  navigator.clipboard.writeText(acct);
+  alert("Account number copied: " + acct);
+}
